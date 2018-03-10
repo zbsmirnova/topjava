5c787b7a8dbf5ef89a270e2d2cef8ce623dfe018
Зинаида <zbsmirnova@gmail.com>
2018-03-10 18:44:59 +0400

    method signature changed, requires userId

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,18 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
+
 public interface MealService {
+    Meal create(Meal meal, int userId);
+
+    void delete(int id, int userId) throws NotFoundException;
+
+    Meal get(int id, int userId) throws NotFoundException;
+
+    void update(Meal meal, int userId);
+
+    List<Meal> getAll(int userId);
 }
