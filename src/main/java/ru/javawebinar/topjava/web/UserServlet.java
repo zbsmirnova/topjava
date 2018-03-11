b9db0abf274304ad2499504be0ffb7b2675a6f5f
Зинаида <zbsmirnova@gmail.com>
2018-03-11 15:29:36 +0400

    sets AuthorizedUser id

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.AuthorizedUser;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +17,13 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("forward to users");
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        String action = request.getParameter("action");
+        if(action.equalsIgnoreCase("setUser")) {
+            AuthorizedUser.setId(Integer.parseInt(request.getParameter("userId")));
+            log.debug("AuthorizedUser id = " + AuthorizedUser.id());
+            request.getRequestDispatcher("meals").forward(request, response);
+        }
+        else request.getRequestDispatcher("/users.jsp").forward(request, response);
+
     }
 }
