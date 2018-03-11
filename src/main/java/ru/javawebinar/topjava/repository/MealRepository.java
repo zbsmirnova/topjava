c3b298c92499cfe2c599fc21e52faed05fd1408c
Зинаида <zbsmirnova@gmail.com>
2018-03-11 15:26:05 +0400

    method signatures changed

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
 import java.util.Collection;
 import java.util.List;
 
@@ -12,5 +14,5 @@ public interface MealRepository {
 
     Meal get(int id, int userId);
 
-    List<Meal> getAll(int userId);
+    List<Meal> getAll(int userId, Temporal startTime, Temporal endTime, Class <? extends Comparable> type);
 }
