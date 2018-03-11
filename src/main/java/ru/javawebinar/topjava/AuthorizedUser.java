5d2eb27edb60adb2b095639ea674842d49b290be
Зинаида <zbsmirnova@gmail.com>
2018-03-11 15:22:47 +0400

    setId added

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -3,12 +3,17 @@ package ru.javawebinar.topjava;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class AuthorizedUser {
+    private static int id;
 
     public static int id() {
-        return 1;
+        return id;
     }
 
     public static int getCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
+    }
+
+    public static void setId(int userId){
+        id = userId;
     }
 }
