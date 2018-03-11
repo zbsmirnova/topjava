3d7807c4eb801c05888a040b45310cb6aaf42cce
Зинаида <zbsmirnova@gmail.com>
2018-03-11 15:27:15 +0400

    method getAll changed - added time filtration

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.Temporal;
 import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
@@ -23,9 +28,34 @@ public class MealRestController{
         this.service = service;
     }
 
-    public List<Meal> getAll(){
+    public <T extends Comparable> List<Meal> getAll(String startDate, String endDate, String startTime, String endTime){
         log.info("getAll");
-        return service.getAll(AuthorizedUser.id());
+        LocalTime localStartTime = startTime ==  null ?  LocalTime.MIN : startTime.equals("") ? LocalTime.MIN : LocalTime.parse(startTime);
+        LocalTime localEndTime = endTime ==  null ?  LocalTime.MAX : endTime.equals("") ? LocalTime.MAX : LocalTime.parse(endTime);
+        LocalDate localStartDate = startDate == null ?  LocalDate.MIN : startDate.equals("") ? LocalDate.MIN : LocalDate.parse(startDate);
+        LocalDate localEndDate = endDate == null ?  LocalDate.MAX : endDate.equals("") ? LocalDate.MAX : LocalDate.parse(endDate);
+
+        Temporal startTemporal;
+        Temporal endTemporal;
+        Class <? extends Comparable> type;
+
+        if(startDate == null && endDate == null || startDate.equals("") && endDate.equals("")){
+            startTemporal = localStartTime;
+            endTemporal = localEndTime;
+            type = LocalTime.class;
+        }
+        else if(startTime == null && endTime == null || startTime.equals("")&& endTime.equals("")){
+            startTemporal = localStartDate;
+            endTemporal = localEndDate;
+            type = LocalDate.class;
+        }
+        else{
+            startTemporal = LocalDateTime.of(localStartDate, localStartTime);
+            endTemporal = LocalDateTime.of(localEndDate, localEndTime);
+            type = LocalDateTime.class;
+        }
+
+        return service.getAll(AuthorizedUser.id(), startTemporal, endTemporal, type);
     }
 
     public Meal get(int id) {
