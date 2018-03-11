84248626a19fb8c02d64335568701988aa71a144
Зинаида <zbsmirnova@gmail.com>
2018-03-11 15:28:08 +0400

    method signatures changed

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
-
+import java.time.temporal.Temporal;
 import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
@@ -40,7 +40,7 @@ public class MealServiceImpl implements MealService {
     }
 
     @Override
-    public List<Meal> getAll(int userId) {
-        return repository.getAll(userId);
+    public List<Meal> getAll(int userId, Temporal startTime, Temporal endTime, Class <? extends Comparable> type) {
+        return repository.getAll(userId, startTime, endTime, type);
     }
 }
