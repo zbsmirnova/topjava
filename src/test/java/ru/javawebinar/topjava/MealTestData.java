4dc2508f9f608528f231cdbcada6d4f4556d9332
Зинаида <zbsmirnova@gmail.com>
2018-03-20 10:17:19 +0400

    MealTestData, InMemoryMealServiceTest, spring-test.xml added

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+
+    public static final int MEAL1_ID = START_SEQ + 2;
+    public static final int MEAL2_ID = START_SEQ + 3;
+    public static final int MEAL3_ID = START_SEQ + 4;
+    public static final int MEAL4_ID = START_SEQ + 5;
+    public static final int MEAL5_ID = START_SEQ + 6;
+    public static final int MEAL6_ID = START_SEQ + 7;
+    public static final int ADMIN_MEAL1_ID = START_SEQ + 8;
+    public static final int ADMIN_MEAL2_ID = START_SEQ + 9;
+
+    public static final Meal ADMIN_MEAL_1 = new Meal(ADMIN_MEAL1_ID, LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510);
+    public static final Meal ADMIN_MEAL_2 = new Meal(ADMIN_MEAL2_ID, LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500);
+    public static final Meal MEAL1 = new Meal(MEAL1_ID, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
+    public static final Meal MEAL2 = new Meal(MEAL2_ID, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000);
+    public static final Meal MEAL3 = new Meal(MEAL3_ID, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500);
+    public static final Meal MEAL4 = new Meal(MEAL4_ID, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000);
+    public static final Meal MEAL5 = new Meal(MEAL5_ID, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500);
+    public static final Meal MEAL6 = new Meal(MEAL6_ID, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510);
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+}
