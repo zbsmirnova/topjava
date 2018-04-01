1622213bec1a589c25533407c4e9dd20b527a3ef
Зинаида <zbsmirnova@gmail.com>
2018-04-01 11:25:37 +0400

    spring-db profiles jdbc,jpa,datajpa

--- src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
+++ src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
@@ -7,6 +7,6 @@ public class ActiveDbProfileResolver implements ActiveProfilesResolver {
 
     @Override
     public String[] resolve(Class<?> aClass) {
-        return new String[]{Profiles.getActiveDbProfile()};
+        return new String[]{Profiles.getActiveDbProfile(), "jdbc"};
     }
 }
