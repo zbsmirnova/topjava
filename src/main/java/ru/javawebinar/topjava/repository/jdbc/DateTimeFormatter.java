3a635fb61251cb8840883aff09178eaf6e821c8b
Зинаида <zbsmirnova@gmail.com>
2018-04-06 12:36:45 +0400

    methods for dataJpa added

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/DateTimeFormatter.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.function.Function;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan({ "ru.javawebinar.topjava.repository.jdbc*" })
+public class DateTimeFormatter {
+
+    @Bean
+    @Profile("hsqldb")
+    public Function<LocalDateTime, Timestamp> convertToTimestamp() {
+        return Timestamp::valueOf;
+    }
+
+    @Bean
+    @Profile("postgres")
+    public Function<LocalDateTime, LocalDateTime> convertToLocalDateTime() {
+        return dt -> dt;
+    }
+}
