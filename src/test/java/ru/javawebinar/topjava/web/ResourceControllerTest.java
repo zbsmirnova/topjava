f8752e593c7396337dbea4d5c1ef9550b88ba04a
Зинаида <zbsmirnova@gmail.com>
2018-04-20 11:49:55 +0400

    ResourceControllerTest added RootControllerTest.testMeals added

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+    @Test
+    public void testStyle() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css"));
+
+
+    }
+}
--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -27,5 +27,21 @@ public class RootControllerTest extends AbstractControllerTest {
                         )
                 )));
     }
+    @Test
+    public void testMeals() throws Exception{
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ+2)),
+                                hasProperty("description", is(MEAL1.getDescription())),
+                                hasProperty("calories", is(MEAL1.getCalories()))
+                        )
+                )));
+    }
 
 }
